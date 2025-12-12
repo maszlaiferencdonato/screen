833c4adf270869850271566246c396e27b666778
maszlaiferencdonato <162970541+maszlaiferencdonato@users.noreply.github.com>
2025-12-12 08:59:49 +0100

    Add files via upload

---
+++ Screen projekt/ConsoleAppScreen/Models/Screen.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppScreen.Models
+{
+    static public class Screen
+    {
+        /// <summary>
+        /// Téglalap rajzolása a képernyőn a megadott koordináták és méretek alapján. 
+        /// </summary>
+        /// <param name="x">X koordináta</param>
+        /// <param name="y">Y koordináta</param>
+        /// <param name="width">A téglalap szélessége</param>
+        /// <param name="height">A téglalap magassága</param>
+        /// <param name="sign">A téglalap rajzolásához használt karakter</param>
+        static public void DrawRectangle(byte x, byte y, byte width, byte height, char sign = '*')
+        {
+            // TODO : (Jancsi) Téglalap rajzolásának implementációja a képernyőn
+        }
+
+        /// <summary>
+        /// Kitöltött téglalap rajzolása a képernyőn a megadott koordináták és méretek alapján.
+        /// </summary>
+        /// <param name="x">X koordináta</param>
+        /// <param name="y">Y koordináta</param>
+        /// <param name="width">A kitöltött téglalap szélessége</param>
+        /// <param name="height">A kitöltött téglalap magassága</param>
+        /// <param name="sign">A kitöltéshez használt karakter</param>
+        static public void FillRectangle(byte x, byte y, byte width, byte height, char sign = '■')
+        {
+            // TODO : (Juliska) Kitöltött téglalap rajzolásának implementációja a képernyőn
+        }
+
+        /// <summary>
+        /// Vonal rajzolása a képernyőn a megadott kezdő és végpontok alapján.
+        /// </summary>
+        /// <param name="x1">X koordináta a kezdőpontban</param>
+        /// <param name="y1">Y koordináta a kezdőpontban</param>
+        /// <param name="x2">X koordináta a végpontban</param>
+        /// <param name="y2">Y koordináta a végpontban</param>
+        /// <param name="sign">A vonal rajzolásához használt karakter</param>
+        static public void DrawLine(byte x1, byte y1, byte x2, byte y2, char sign = '*')
+        {
+            // TODO : (jancsi) Vonal rajzolásának implementációja a képernyőn
+        }
+
+        /// <summary>
+        /// Adott szélességre középre igazítja a szöveget
+        /// </summary>
+        /// <param name="text">A középre igazítandó szöveg</param>
+        /// <param name="width">A szélesség, amire igazítani kell</param>
+        /// <returns>A szöveg középre igazított változata</returns>
+        static public string AlignTextCenter(string text, int width)
+        {
+            // TODO : (Juliska) Szöveg középre igazításának implementációja
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Keverve adja vissza a két szöveg karaktereit.
+        /// </summary>
+        /// <param name="textA">Az első szöveg.</param>
+        /// <param name="textB">A második szöveg.</param>
+        /// <returns>A két szöveg karaktereinek keverésével elkészített szöveg</returns>
+        public static string MixedStrings(string textA, string textB)
+        {
+            // TODO : (Jancsi) Két szöveg keverésének implementációja
+            // 1. példa:
+            // textA = "Hello"
+            // textB = "World"
+            // Kimenet: HWeolrllod
+
+            // 2. példa:
+            // textA = "abcd"
+            // textB = "12345"
+            // Kimenet: a1b2c3d45
+            throw new NotImplementedException();
+        }
+
+        // TODO : (Juliska) Két szöveg ismételt váltakozásának implementációja
+        /// <summary>
+        /// Egymás után váltakozva szereplő szövegeket fűz egybe.
+        /// </summary>
+        /// <param name="textA">Az első szöveg.</param>
+        /// <param name="textB">A második szöveg.</param>
+        /// <param name="iteration">A fűzések ismétlési száma.</param>
+        /// <returns>A két szöveg ismételt váltakozásával elkészített szöveg</returns>
+        public static string RepeatedStrings(string textA, string textB, int iteration)
+        {
+            // példa:
+            // textA = "Hi"
+            // textB = "There"
+            // iteration = 3
+            // Kimenet: HiThereHiThereHiThere
+
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Screen projekt/ConsoleAppScreen/Program.cs
@@ -0,0 +1,28 @@
+﻿using ConsoleAppScreen.Models;
+
+namespace ConsoleAppScreen
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Például egy téglalap rajzolása a képernyőn
+            Screen.DrawRectangle(10, 5, 20, 10);
+            // Például egy kitöltött téglalap rajzolása a képernyőn
+            Screen.FillRectangle(40, 5, 20, 10);
+            // Például egy vonal rajzolása a képernyőn
+            Screen.DrawLine(10, 20, 60, 20);
+            // Például szöveg középre igazítása
+            string centeredText = Screen.AlignTextCenter("Középre igazított szöveg", 50);
+            Console.WriteLine(centeredText);
+            // Például két szöveg keverése
+            string mixedText = Screen.MixedStrings("Hello", "World");
+            Console.WriteLine(mixedText);
+            // Például két szöveg ismétlése
+            string repeatedText = Screen.RepeatedStrings("AB", "12", 5);
+            Console.WriteLine(repeatedText);
+
+
+        }
+    }
+}
